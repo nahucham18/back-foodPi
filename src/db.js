9d709cde47e9735ff24b047f0f12236ece5a8e1c
nahucham18 <nahu.chamo19@gmail.com>
2023-04-27 14:50:56 -0300

    Deploy back

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY,DATA_BASE, DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST,DATA_BASE, DB_PORT,DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATA_BASE}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
